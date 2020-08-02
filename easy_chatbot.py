6b968f0cfd5d1ad5ac84efe4cd3f725feee7d49f
Daniel Salzedo <66858971+dsalzedon@users.noreply.github.com>
2020-08-02 17:41:53 -0500

    Update easy_chatbot.py

--- easy_chatbot.py
+++ easy_chatbot.py
@@ -1,4 +1,6 @@
 """
+SIMPLE CHATTY BOT
+
 About
 Here, at the beginning of your programmer’s path, creating a simple console chat bot will do wonders to guide you through the basics of coding. 
 During this journey you will also play some word and number games that you are going to implement all on your own. 
