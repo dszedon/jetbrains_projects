683e28d29c73554b59c3a4eb3f81da7e5f2d5f88
Daniel Salzedo <66858971+dsalzedon@users.noreply.github.com>
2020-08-02 17:56:13 -0500

    Update medium_numeric_matrix_processor.py

--- medium_numeric_matrix_processor.py
+++ medium_numeric_matrix_processor.py
@@ -5,6 +5,9 @@ Here’s a project for devoted matrix enthusiasts:
 learn to perform a variety of operations on matrices including addition, multiplication, finding the determinant, and dealing with inverse matrices. 
 If you are working on your tech or math major, this project is a good chance for you to learn matrices in action and not just in your notebook.
 
+Learning outcomes
+Apart from learning a whole lot about matrices, you will become familiar with the Math library, recursion, and the many ways of using nested lists in practice.
+
 Stage 1
 Use nested lists to add matrices.
 
