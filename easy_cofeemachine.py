1e8903e23779f1b59436a86509352613f9b868a9
Daniel Salzedo <66858971+dsalzedon@users.noreply.github.com>
2020-08-02 17:55:17 -0500

    Update easy_cofeemachine.py

--- easy_cofeemachine.py
+++ easy_cofeemachine.py
@@ -7,6 +7,7 @@ It’s enough to press a couple of buttons on the machine and you get a cup of e
 In this project, you will work on programming a coffee machine simulator. 
 The machine works with typical products: coffee, milk, sugar, and plastic cups; if it runs out of something, it shows a notification. 
 You can get three types of coffee: espresso, cappuccino, and latte. Since nothing’s for free, it also collects the money.
+
 Learning outcomes
 This project allows you to get a taste of Python. 
 Practice working with functions, challenge yourself with loops and conditions, and get more confident in Python.
