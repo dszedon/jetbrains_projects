f5423bb4e2130d12ea97f018e23b4cfeb1e0a494
Daniel Salzedo <66858971+dsalzedon@users.noreply.github.com>
2020-08-02 17:44:16 -0500

    Update easy_cofeemachine.py

--- easy_cofeemachine.py
+++ easy_cofeemachine.py
@@ -1,32 +1,35 @@
 """
-Description
+COFEE MACHINE
 
-Let's redesign our program and write a class that represents the coffee machine. 
-The class should have a method that takes a string as input. 
-Every time the user inputs a string to the console, the program invokes this method with one argument: 
-the line that user input to the console. This system simulates pretty accurately how real-world electronic devices work. 
-External components (like buttons on the coffee machine or tapping on the screen) generate events that pass into the single interface of the program.
+About
+What can be better than a cup of coffee during a break? A coffee that you don’t have to make yourself. 
+It’s enough to press a couple of buttons on the machine and you get a cup of energy; but first, we should teach the machine how to do it. 
+In this project, you will work on programming a coffee machine simulator. 
+The machine works with typical products: coffee, milk, sugar, and plastic cups; if it runs out of something, it shows a notification. 
+You can get three types of coffee: espresso, cappuccino, and latte. Since nothing’s for free, it also collects the money.
+Learning outcomes
+This project allows you to get a taste of Python. 
+Practice working with functions, challenge yourself with loops and conditions, and get more confident in Python.
 
-The class should not use system input at all; 
-it will only handle the input that comes to it via this method and its string argument.
+Stage 1
+Write a program that puts basic information on the screen: give the machine a chance to tell the customers what it’s doing!
 
-The first problem that comes to mind: 
-how to write that method in a way that it represents all that coffee machine can do? 
-If the user inputs a single number, how can the method determine what that number is: 
-a variant of coffee chosen by the user or the number of the disposable cups that a special worker added into the coffee machine?
+Stage 2
+Program the machine to calculate the amount of ingredients it needs depending on how many people want some coffee.
 
-The right solution to this problem is to store the current state of the machine. 
-The coffee machine has several states it can be in. 
-For example, the state could be "choosing an action" or "choosing a type of coffee". 
-Every time the user inputs something and a program passes that line to the method, 
-the program determines how to interpret this line using the information about the current state. 
-After processing this line, the state of the coffee machine can be changed or can stay the same.
+Stage 3
+Working with conditions, program the machine to estimate how many creamy coffees it can make based on the amount of ingredients we enter.
 
-Objective
+Stage 4
+Upgrade your knowledge of functions – set the machine to perform three basic actions: 
+collect the money, renew the supplies, and serve the coffee.
 
-Your final task is to refactor the program. 
-Make it so that you can communicate with the coffee machine through a single method. 
-Good luck!
+Stage 5
+Program the machine to display on the screen the amount of supplies left. 
+Set the main loop: now the menu keeps updating until you enter “exit”.
+
+Stage 6
+Time for some final touch-ups: structure the code so that it runs smoothly.
 
 """
 class CoffeeType:
