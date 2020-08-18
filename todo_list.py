6a4af625185e2b944cefe22cce1edca877b5e869
Daniel Salzedo <66858971+dsalzedon@users.noreply.github.com>
2020-08-17 22:19:07 -0500

    Create todo_list.py

---
+++ todo_list.py
@@ -0,0 +1,16 @@
+"""
+
+To-Do List 
+
+About  
+To-Do list can improve your work and personal life. 
+You can use it to reduce the stress in your life and get more done in less time. 
+It also helps you become more reliable for other people and save time for the best things in life. 
+So let's implement it!
+
+Learning outcomes  
+To-Do list that will help you manage your tasks. 
+You will practice using loops, conditions and statement branches. 
+And also you will learn the basics of SQLAlchemy to manage SQLite database on python!
+
+"""
